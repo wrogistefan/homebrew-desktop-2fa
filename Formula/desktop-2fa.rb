2a103d8bd6ee864a76a2a2c5f23b19a92984931b
wrogistefan <lukasz.perek@outlook.com>
2026-01-07 18:16:51 +0100

    fix

--- Formula/desktop-2fa.rb
+++ Formula/desktop-2fa.rb
@@ -5,7 +5,7 @@ class Desktop2fa < Formula
   homepage "https://github.com/wrogistefan/desktop-2fa"
   url "https://files.pythonhosted.org/packages/source/d/desktop_2fa/desktop_2fa-0.7.1.tar.gz"
   sha256 "e975c4dcd0531610c61acf0ae28babca4e87845c96fe7dbd45dbb13907a97f73"
-  license "MIT"
+  license "Apache-2.0"
 
   depends_on "python@3.12"
   depends_on "pipx"
