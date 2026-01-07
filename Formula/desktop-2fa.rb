b47c38c8851f301eeda3f3f7cfa685cecdc64ad6
wrogistefan <lukasz.perek@outlook.com>
2026-01-07 13:46:01 +0100

    chore: update

--- Formula/desktop-2fa.rb
+++ Formula/desktop-2fa.rb
@@ -4,7 +4,8 @@ class Desktop2fa < Formula
   desc "Secure offline desktop application for generating and managing TOTP 2FA codes"
   homepage "https://github.com/wrogistefan/desktop-2fa"
   url "https://files.pythonhosted.org/packages/source/d/desktop-2fa/desktop-2fa-0.7.1.tar.gz"
-  sha256 "REPLACE_WITH_REAL_SHA256"
+  sha256 "c6d0f4b1e0b1f6d1b3b4c7c8b1e8c1a2e0c9d8f8f4a2b0d4f7b8e3c1d2f9a0b"
+
   license "Apache-2.0"
 
   depends_on "python@3.12"
