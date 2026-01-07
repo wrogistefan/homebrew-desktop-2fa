2e46119e70223cdad176ee5cd406ce867e9bdef3
Lukasz Perek <lukasz.perek@outlook.com>
2026-01-08 00:37:47 +0100

    Update desktop-2fa.rb
    release 0.7.2

--- Formula/desktop-2fa.rb
+++ Formula/desktop-2fa.rb
@@ -1,15 +1,15 @@
 class Desktop2fa < Formula
   desc "Secure TOTP authenticator for desktop with encrypted vault"
   homepage "https://github.com/wrogistefan/desktop-2fa"
-  url "https://files.pythonhosted.org/packages/source/d/desktop_2fa/desktop_2fa-0.7.1.tar.gz"
-  sha256 "e975c4dcd0531610c61acf0ae28babca4e87845c96fe7dbd45dbb13907a97f73"
+  url "https://files.pythonhosted.org/packages/source/d/desktop_2fa/desktop_2fa-0.7.2.tar.gz"
+  sha256 "009C605476CD656EF9FD14A21CD2E61E1932881BD76458DF6A3161FCE95E9702"
   license "Apache-2.0"
 
   depends_on "python@3.12"
   depends_on "pipx"
 
   def install
-    system "pipx", "install", "desktop-2fa==0.7.1"
+    system "pipx", "install", "desktop-2fa==0.7.2"
     (bin/"d2fa").write <<~EOS
       #!/bin/bash
       exec pipx run desktop-2fa "$@"
