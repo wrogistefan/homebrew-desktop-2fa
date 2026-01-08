60f0d400f554475eb9c4c451880e38e4bcf9df2d
Lukasz Perek <lukasz.perek@outlook.com>
2026-01-08 18:46:47 +0100

    Update desktop-2fa.rb

--- Formula/desktop-2fa.rb
+++ Formula/desktop-2fa.rb
@@ -1,22 +1,19 @@
 class Desktop2fa < Formula
-  desc "Secure TOTP authenticator for desktop with encrypted vault"
+  include Language::Python::Virtualenv
+
+  desc "Desktop TOTP authenticator"
   homepage "https://github.com/wrogistefan/desktop-2fa"
-  url "https://files.pythonhosted.org/packages/source/d/desktop_2fa/desktop_2fa-0.7.2.tar.gz"
+  url "https://files.pythonhosted.org/packages/source/d/desktop-2fa/desktop-2fa-0.7.2.tar.gz"
   sha256 "009C605476CD656EF9FD14A21CD2E61E1932881BD76458DF6A3161FCE95E9702"
   license "Apache-2.0"
 
   depends_on "python@3.12"
-  depends_on "pipx"
 
   def install
-    system "pipx", "install", "desktop-2fa==0.7.2"
-    (bin/"d2fa").write <<~EOS
-      #!/bin/bash
-      exec pipx run desktop-2fa "$@"
-    EOS
+    virtualenv_install_with_resources
   end
 
   test do
-    system "#{bin}/d2fa", "--help"
+    assert_match "Desktop-2FA v0.7.2", shell_output("#{bin}/d2fa --version")
   end
 end
